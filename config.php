7ce7ee592788e5ab9da948782ad1a5cf51228876
qjlxg <qjlxg@163.com>
2024-10-12 17:14:15 +0800

    Update config.php

--- config.php
+++ config.php
@@ -8,5 +8,19 @@ $subscription_urls = [
   "https://raw.githubusercontent.com/mianfeifq/share/main/data2023094.txt",
   "https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2",
   "https://raw.githubusercontent.com/freefq/free/master/v2",
+  "https://github.com/qjlxg/YBSub/raw/refs/heads/master/sub/share/available",
+"https://github.com/qjlxg/YBSub/raw/refs/heads/master/sub/share/ss",
+"https://github.com/qjlxg/YBSub/raw/refs/heads/master/sub/share/ssr",
+"https://github.com/qjlxg/YBSub/raw/refs/heads/master/sub/share/tr",
+"https://github.com/qjlxg/YBSub/raw/refs/heads/master/sub/share/v2",
+"https://github.com/qjlxg/Sub2/raw/refs/heads/main/sub/share/available",
+"https://github.com/qjlxg/Sub2/raw/refs/heads/main/sub/share/ss",
+"https://github.com/qjlxg/Sub2/raw/refs/heads/main/sub/share/ssr",
+"https://github.com/qjlxg/Sub2/raw/refs/heads/main/sub/share/tr",
+"https://github.com/qjlxg/YBSubCrawler/raw/refs/heads/main/sub/share/available",
+"https://github.com/qjlxg/YBSubCrawler/raw/refs/heads/main/sub/share/ss",
+"https://github.com/qjlxg/YBSubCrawler/raw/refs/heads/main/sub/share/ssr",
+"https://github.com/qjlxg/YBSubCrawler/raw/refs/heads/main/sub/share/tr",
+
   "https://test2.sub-channel.workers.dev/"
 ];
